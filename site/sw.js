27d6918b838d5e64c16d3ef3c2ffb139637d339e
Muntasim Ul Haque <106990977+muntasimulhaque@users.noreply.github.com>
2026-07-16 20:26:24 +0600

    Update cache version to count-play-v7Bump service worker cache to v7

--- site/sw.js
+++ site/sw.js
@@ -1,4 +1,4 @@
-var CACHE = "count-play-v6";
+var CACHE = "count-play-v7";
 var ASSETS = ["./", "./index.html", "./manifest.json",
               "./icons/icon-192.png", "./icons/icon-512.png", "./icons/icon-maskable-512.png"];
 
