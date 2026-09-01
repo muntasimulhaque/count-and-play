62acf0257706939d600d13ea6979819fce8282aa
Muntasim Ul Haque <muntasim.haque@gmail.com>
2026-09-01 17:51:56 +0600

    Release 8.4 (versionCode 31)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "app.maqsadah.count_and_play.twa"
         minSdk = 23
         targetSdk = 37
-        versionCode = 30
-        versionName = "8.3"
+        versionCode = 31
+        versionName = "8.4"
 
         // Instrumented tests (the emulator screenshot capture) use AndroidX's runner.
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
